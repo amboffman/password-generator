f913fca556a48b8800968257e0827d51adbc6947
Anthony Boffman <amarrokalboffman@gmail.com>
2020-03-11 16:53:18 -0700

    Add functions to check if they want special characters, lowercase, uppercase, and/or numbers

--- script.js
+++ script.js
@@ -22,19 +22,36 @@ function generatePassword() {
 // on click, prompt user to input desired password length
 var numPasswordCharacters = prompt("How many characters would you like your password to be?");
 // Check if password length is between 8 and 128
+// If password length is not between 8 and 128
 if(numPasswordCharacters>=passwordCharacterMin && numPasswordCharacters<=passwordCharacterMax){
-  alert("You can proceed.");
+  lowerCaseCharacterCheck();
+  specialCharacterCheck();
+  upperCaseCharacterCheck();
+  numericCharacterCheck();
+
 }
 else {
+  // prompt user to input password between 8 and 128
   alert("Please input a number between " + passwordCharacterMin +" and " + passwordCharacterMax);
 }
-// If password length is not between 8 and 128
-// prompt user to input password between 8 and 128
+// then confirm if they want lower case characters
+function lowerCaseCharacterCheck(){
+  confirm("Would you like lowercase characters in your password?")
+}
 // then confirm if they want special characters
+function specialCharacterCheck(){
+  confirm("Would you like special characters in your password?")
+}
 // If so, add special characters array to password array
 // then confirm if they want upper case letters
+function upperCaseCharacterCheck(){
+  confirm("Would you like uppercase characters in your password?")
+}
 // If so, add upper case letters array to password array
 // then confirm if they want numerics
+function numericCharacterCheck(){
+  confirm("Would you like numbers in your password?")
+}
 // If so, add numerics array to password array
 // validate at least one criteria has been selected
 // add the desired password legnth of characters from the password array into random password array
