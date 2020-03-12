36be98e388eb781d5ee717997f5697edea899283
Anthony Boffman <amarrokalboffman@gmail.com>
2020-03-11 17:25:46 -0700

    Add arrays for each type of character and a 'password array'. As user confirms which types of character arrays they want, the program pushes those characters into the password array.

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 var passwordCharacterMax = 128;
 var passwordCharacterMin = 8;
+var lowerCaseLetters = ["a","b", "c"];
+var upperCaseLetters = ["A", "B", "C"];
+var numbers = ["1","2","3"];
+var specialCharacters = ["!", "@", "#"];
+var passwordArray =[];
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -36,25 +43,48 @@ else {
 }
 // then confirm if they want lower case characters
 function lowerCaseCharacterCheck(){
-  confirm("Would you like lowercase characters in your password?")
+  var lowerCaseLettersWanted = confirm("Would you like lowercase characters in your password?");
+    // If so, add numerics array to password array
+  if(lowerCaseLettersWanted){
+    for(i=0;i<lowerCaseLetters.length; i++){
+      passwordArray.push(lowerCaseLetters[i]);
+      }
+  }
 }
 // then confirm if they want special characters
 function specialCharacterCheck(){
-  confirm("Would you like special characters in your password?")
+  var specialCharactersWanted = confirm("Would you like special characters in your password?");
+  // If so, add special characters array to password array
+  if(specialCharactersWanted){
+    for(j=0;j<specialCharacters.length; j++){
+      passwordArray.push(specialCharacters[j]);
+      }
+  }
 }
-// If so, add special characters array to password array
 // then confirm if they want upper case letters
 function upperCaseCharacterCheck(){
-  confirm("Would you like uppercase characters in your password?")
+  var upperCaseLettersWanted = confirm("Would you like uppercase characters in your password?");
+  // If so, add upper case letters array to password array
+  if(upperCaseLettersWanted){
+    for(k=0;k<upperCaseLetters.length; k++){
+      passwordArray.push(upperCaseLetters[k]);
+      }
+  }
 }
-// If so, add upper case letters array to password array
 // then confirm if they want numerics
 function numericCharacterCheck(){
-  confirm("Would you like numbers in your password?")
+  var numbersWanted = confirm("Would you like numbers in your password?");
+  // If so, add numerics array to password array
+  if(numbersWanted){
+    for(l=0;i<numbers.length; l++){
+      passwordArray.push(numbers[l]);
+      }
+  }
 }
-// If so, add numerics array to password array
 // validate at least one criteria has been selected
 // add the desired password legnth of characters from the password array into random password array
 // verify one of each criteria is in random password array
 // loop until one of each criteria is in the randomly  password array
 // display the string in the box
+
+console.log(passwordArray);
