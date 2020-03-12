368167cf1b432ddab12eaf1221f4544fc491232c
Anthony Boffman <amarrokalboffman@gmail.com>
2020-03-11 20:10:20 -0700

    Verify that at least one criteria was chosen, and alert them if not

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordCharacterMax = 128;
-var passwordCharacterMin = 8;
+var passwordCharacterMin = 1;
 var lowerCaseLetters = ["a","b", "c"];
 var upperCaseLetters = ["A", "B", "C"];
 var numbers = ["1","2","3"];
 var specialCharacters = ["!", "@", "#"];
-var passwordArray =[1];
+var passwordArray =[];
 var randomPasswordArray =[];
 var includeLowerCase = false;
 var includeUpperCase = false;
 var includeSpecial = false;
 var includeNumber = false;
+var passwordHasLowerCase = false;
+var passwordHasUpperCase = false;
+var passwordHasSpecial = false;
+var passwordHasNumber = false;
+var finalPassword = [];
 
 
 
@@ -35,15 +40,25 @@ function generatePassword() {
 // on click, prompt user to input desired password length
 var numPasswordCharacters = prompt("How many characters would you like your password to be?");
 // Check if password length is between 8 and 128
-// If password length is not between 8 and 128
 if(numPasswordCharacters>=passwordCharacterMin && numPasswordCharacters<=passwordCharacterMax){
+  // then confirm if they want lower case characters
   lowerCaseCharacterCheck();
+  // then confirm if they want special characters
   specialCharacterCheck();
+  // then confirm if they want upper case letters
   upperCaseCharacterCheck();
+  // then confirm if they want numerics
   numericCharacterCheck();
+  // validate at least one criteria has been selected
   atLeastOneCritera();
-
+  // add the desired password length of characters from the password array into random password array
+  // verify one of each criteria is in random password array
+  arrayCriteriaMatch();
+  // verify password length matches requested length
+  // produce password to screen
+  
 }
+// If password length is not between 8 and 128
 else {
   // prompt user to input password between 8 and 128
   alert("Please input a number between " + passwordCharacterMin +" and " + passwordCharacterMax);
@@ -86,7 +101,7 @@ function numericCharacterCheck(){
   var numbersWanted = confirm("Would you like numbers in your password?");
   // If so, add numerics array to password array
   if(numbersWanted){
-    for(l=0;i<numbers.length; l++){
+    for(l=0;l<numbers.length; l++){
       passwordArray.push(numbers[l]);
       includeNumber = true;
       }
@@ -95,13 +110,52 @@ function numericCharacterCheck(){
 // validate at least one criteria has been selected
 function atLeastOneCritera(){
   if(includeNumber||includeSpecial||includeUpperCase||includeLowerCase){
-    for(m=0;m<numPasswordCharacters;m++){
-    // add the desired password legnth of characters from the password array into random password array
+    for(m=0;m<passwordArray.length; m++){
+    // add the desired password length of characters from the password array into random password array
       randomPasswordArray.push(passwordArray[Math.floor(Math.random()*passwordArray.length)]);
     }
+    for(n=0;n<((numPasswordCharacters/2)-1); n++){
+      finalPassword.push(randomPasswordArray[n]);
+    }
+    }
+  else{
+    alert("Please choose at least one character type to include in your password.")
   }
-}
-// verify one of each criteria is in random password array
+  }
+
+function arrayCriteriaMatch(){
+  if(includeLowerCase){
+    for(o=0; o<lowerCaseLetters.length;o++){
+      if(randomPasswordArray.includes(lowerCaseLetters[o])){
+        passwordHasLowerCase = true;
+      }
+      else{
+        atLeastOneCritera();
+      }
+    }
+  }
+  if(includeUpperCase){
+    for(p=0; p<upperCaseLetters.length;p++){
+      if(randomPasswordArray.includes(upperCaseLetters[p])){
+        passwordHasUpperCase = true;
+      }
+    }
+  }
+  if(includeSpecial){
+    for(q=0; q<specialCharacters.length;q++){
+      if(randomPasswordArray.includes(specialCharacters[q])){
+        passwordHasSpecial = true;
+      }
+    }
+  }
+  if(includeNumber){
+    for(r=0; r<numbers.length;r++){
+      if(randomPasswordArray.includes(numbers[r])){
+        passwordHasNumber = true;
+      }
+    }
+  }
+  }
 // loop until one of each criteria is in the randomly  password array
 // display the string in the box
 
@@ -109,3 +163,11 @@ console.log("passwordArray: " + passwordArray);
 console.log("RandompasswordArray: " + randomPasswordArray);
 console.log(includeLowerCase);
 console.log(numPasswordCharacters);
+console.log("Password has lowercase letter: " + passwordHasLowerCase);
+console.log("Password has uppercase letter: " + passwordHasUpperCase);
+console.log("Password has special character: " + passwordHasSpecial);
+console.log("Password has number: " + passwordHasNumber);
+console.log("Include Number?: " + includeNumber);
+console.log("Include Uppercase: " + includeUpperCase);
+console.log("Final Password: " + finalPassword);
+console.log("Final Password Length: " + finalPassword.length);
